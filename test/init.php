bcc43349123659688035ba92cea64ee6223c3770
Peter Aba <devmonk@devmonk.com>
2014-01-26 19:07:46 +0100

    Fixing test runners

--- test/init.php
+++ test/init.php
@@ -9,5 +9,3 @@ if (file_exists('vendor/autoload.php')) {
 } else {
     throw new RuntimeException('vendor/autoload.php could not be found. Did you run `php composer.phar install`?');
 }
-
-$loader->add('DmFileman', __DIR__ . '/src');
